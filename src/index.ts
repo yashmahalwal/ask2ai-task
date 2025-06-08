b7f22a69194071845367341337cd7d7995ea39da
yashmahalwal <yashmahalwal@gmail.com>
2025-06-08 13:49:36 +0530

    chore: add support for subscriptions

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,74 @@
 // Load env vars - must be the first import
 import "dotenv/config";
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware } from "@apollo/server/express4";
+import express from "express";
+import { createServer } from "http";
 import { typeDefs } from "./schema";
 import { resolvers } from "./graphql/resolvers";
 import { initialiseStorage } from "./storage/initialise-storage";
 import { logger } from "./utils/logger";
 import { isDev } from "./utils/environment";
-
-// The ApolloServer constructor requires two parameters: your schema
-// definition and your set of resolvers.
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+import { useServer } from "graphql-ws/lib/use/ws";
+import { WebSocketServer } from "ws";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import cors from "cors";
+import { ApolloServerPluginLandingPageLocalDefault } from "@apollo/server/plugin/landingPage/default";
 
 async function main() {
-  // Start database
   logger.debug("Setting up database");
   await initialiseStorage();
 
-  // Read and parse the port
-  const port = process.env.PORT && parseInt(process.env.PORT);
+  const port = (process.env.PORT && parseInt(process.env.PORT)) || 4000;
 
-  // Set up graphql server
-  logger.debug("Starting GraphQL server");
+  const app = express();
+  const httpServer = createServer(app);
 
-  const { url } = await startStandaloneServer(server, {
-    // Fallback to port 4000
-    listen: { port: isNaN(port) ? 4000 : port },
+  const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+  // Set up WebSocket server for subscriptions
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: "/subscriptions",
   });
-  logger.info(`🚀  Server ready at: ${url}`);
-  logger.debug(`🌍  Environment: ${isDev() ? "Development" : "Production"}`);
-}
 
-// Start the server
+  // Save the returned server's info so we can shutdown this server later
+  const wsServerCleanup = useServer({ schema }, wsServer);
+
+  const graphqlServer = new ApolloServer({
+    schema,
+    plugins: [
+      // Proper shutdown for the HTTP server.
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+
+      // Proper shutdown for the WebSocket server.
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await wsServerCleanup.dispose();
+            },
+          };
+        },
+      },
+      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
+    ],
+  });
+
+  await graphqlServer.start();
+  app.use(
+    "/graphql",
+    cors<cors.CorsRequest>(),
+    express.json(),
+    expressMiddleware(graphqlServer)
+  );
+
+  httpServer.listen(port, () => {
+    logger.info(`🚀  Server ready at: http://localhost:${port}/graphql`);
+    logger.debug(`🌍  Environment: ${isDev() ? "Development" : "Production"}`);
+  });
+}
 
 main().catch((error) => {
   logger.error(error);
